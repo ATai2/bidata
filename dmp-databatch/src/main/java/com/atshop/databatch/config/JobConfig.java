4b668865a7182677cccb1fac853581b4c722ac5c
atai2 <1114677814@qq.com>
2021-11-18 22:46:16 +0800

    ddd

--- dmp-databatch/src/main/java/com/atshop/databatch/config/JobConfig.java
+++ dmp-databatch/src/main/java/com/atshop/databatch/config/JobConfig.java
@@ -1,8 +1,16 @@
 package com.atshop.databatch.config;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.context.annotation.Configuration;
 
+@EnableBatchProcessing
 @Configuration
 public class JobConfig {
+
+    private JobBuilderFactory jobBuilderFactory;
+    private StepBuilderFactory stepBuilderFactory;
+
 
 }
