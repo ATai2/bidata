60b04aa467a658eab6b28c8c0182f866e36371c5
atai2 <1114677814@qq.com>
2021-11-19 00:07:42 +0800

    ddd

--- dmp-databatch/src/main/java/com/atshop/databatch/config/FlowConfig.java
+++ dmp-databatch/src/main/java/com/atshop/databatch/config/FlowConfig.java
@@ -6,70 +6,61 @@ import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.builder.FlowBuilder;
+import org.springframework.batch.core.job.flow.Flow;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@EnableBatchProcessing
 @Configuration
-public class JobConfig {
+@EnableBatchProcessing
+public class FlowConfig {
 
+    @Autowired
     private JobBuilderFactory jobBuilderFactory;
+    @Autowired
     private StepBuilderFactory stepBuilderFactory;
 
-    public JobConfig(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory) {
-        this.jobBuilderFactory = jobBuilderFactory;
-        this.stepBuilderFactory = stepBuilderFactory;
-    }
-
     @Bean
-    public Job helloJob() {
-        return jobBuilderFactory.get("helloJob")
-                .start(step1())
-                .next(step2())
-                .next(step3())
-                .build();
-    }
-
-    @Bean
-    public Step step1() {
-        return stepBuilderFactory.get("step")
+    public Step flowStep1() {
+        return stepBuilderFactory.get("flowstep1")
                 .tasklet(new Tasklet() {
                     @Override
                     public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-                        System.out.println("hello");
+                        System.out.println("flowstep1");
                         return RepeatStatus.FINISHED;
                     }
-                })
+                }).build();
+    }
+
+    @Bean
+    public Step flowStep2() {
+        return stepBuilderFactory.get("flowstep2")
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println("flowstep2");
+                        return RepeatStatus.FINISHED;
+                    }
+                }).build();
+    }
+
+    @Bean
+    public Flow flowDemo() {
+        return new FlowBuilder<Flow>("flowDemo")
+                .start(flowStep1())
+                .next(flowStep2())
                 .build();
     }
 
     @Bean
-    public Step step2() {
-        return stepBuilderFactory.get("step2")
-                .tasklet(new Tasklet() {
-                    @Override
-                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-                        System.out.println("hello2");
-                        return RepeatStatus.FINISHED;
-                    }
-                })
+    public Job flowJob() {
+        return jobBuilderFactory.get("flowJob")
+                .start(flowDemo())
+                .end()
                 .build();
     }
-
-    @Bean
-    public Step step3() {
-        return stepBuilderFactory.get("step3")
-                .tasklet(new Tasklet() {
-                    @Override
-                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-                        System.out.println("hello3");
-                        return RepeatStatus.FINISHED;
-                    }
-                })
-                .build();
-    }
-
 }
