134a92ede411871c870235844849f89133ddc381
atai2 <1114677814@qq.com>
2021-11-20 13:00:34 +0800

    batch listner

--- dmp-databatch/src/main/java/com/atshop/databatch/config/JobParamConfig.java
+++ dmp-databatch/src/main/java/com/atshop/databatch/config/JobParamConfig.java
@@ -1,67 +1,67 @@
-package com.atshop.databatch.config;
-
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepContribution;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.scope.context.ChunkContext;
-import org.springframework.batch.core.step.tasklet.Tasklet;
-import org.springframework.batch.repeat.RepeatStatus;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@EnableBatchProcessing
-@Configuration
-public class JobParamConfig {
-    @Autowired
-    private JobBuilderFactory jobBuilderFactory;
-    @Autowired
-    private StepBuilderFactory stepBuilderFactory;
-
-
-    @Bean
-    public Job paramJob(){
-        return jobBuilderFactory.get("ParamJpb")
-                .start(step1())
-                .build();
-    }
-    @Bean
-    public Step step1() {
-        return stepBuilderFactory.get("step")
-                .tasklet((stepContribution, chunkContext) -> {
-                    System.out.println("hello");
-                    return RepeatStatus.FINISHED;
-                })
-                .build();
-    }
-
-    @Bean
-    public Step step2() {
-        return stepBuilderFactory.get("step2")
-                .tasklet(new Tasklet() {
-                    @Override
-                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-                        System.out.println("hello2");
-                        return RepeatStatus.FINISHED;
-                    }
-                })
-                .build();
-    }
-
-    @Bean
-    public Step step3() {
-        return stepBuilderFactory.get("step3")
-                .tasklet(new Tasklet() {
-                    @Override
-                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-                        System.out.println("hello3");
-                        return RepeatStatus.FINISHED;
-                    }
-                })
-                .build();
-    }
-
-}
+//package com.atshop.databatch.config;
+//
+//import org.springframework.batch.core.Job;
+//import org.springframework.batch.core.Step;
+//import org.springframework.batch.core.StepContribution;
+//import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+//import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+//import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+//import org.springframework.batch.core.scope.context.ChunkContext;
+//import org.springframework.batch.core.step.tasklet.Tasklet;
+//import org.springframework.batch.repeat.RepeatStatus;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@EnableBatchProcessing
+//@Configuration
+//public class JobParamConfig {
+//    @Autowired
+//    private JobBuilderFactory jobBuilderFactory;
+//    @Autowired
+//    private StepBuilderFactory stepBuilderFactory;
+//
+//
+//    @Bean
+//    public Job paramJob(){
+//        return jobBuilderFactory.get("ParamJpb")
+//                .start(step1())
+//                .build();
+//    }
+//    @Bean
+//    public Step step1() {
+//        return stepBuilderFactory.get("step")
+//                .tasklet((stepContribution, chunkContext) -> {
+//                    System.out.println("hello");
+//                    return RepeatStatus.FINISHED;
+//                })
+//                .build();
+//    }
+//
+//    @Bean
+//    public Step step2() {
+//        return stepBuilderFactory.get("step2")
+//                .tasklet(new Tasklet() {
+//                    @Override
+//                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+//                        System.out.println("hello2");
+//                        return RepeatStatus.FINISHED;
+//                    }
+//                })
+//                .build();
+//    }
+//
+//    @Bean
+//    public Step step3() {
+//        return stepBuilderFactory.get("step3")
+//                .tasklet(new Tasklet() {
+//                    @Override
+//                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+//                        System.out.println("hello3");
+//                        return RepeatStatus.FINISHED;
+//                    }
+//                })
+//                .build();
+//    }
+//
+//}
