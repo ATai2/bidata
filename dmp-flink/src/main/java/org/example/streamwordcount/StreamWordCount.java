ac0ac5d2348a84900b72409c842006ea7465b2f1
atai2 <1114677814@qq.com>
2021-12-04 19:46:27 +0800

    read from mysql

--- dmp-flink/src/main/java/org/example/streamwordcount/StreamWordCount.java
+++ dmp-flink/src/main/java/org/example/streamwordcount/StreamWordCount.java
@@ -5,23 +5,58 @@ import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.api.java.operators.AggregateOperator;
 import org.apache.flink.api.java.operators.DataSource;
 import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.util.Collector;
 
 public class StreamWordCount {
     public static void main(String[] args) throws Exception {
-        ExecutionEnvironment env=ExecutionEnvironment.getExecutionEnvironment();
+        StreamExecutionEnvironment env= StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(8);
         String inputPath = StreamWordCount.class.getClassLoader().getResource("hello.txt").getPath();
-        DataSource<String> stringDataSource = env.readTextFile(inputPath);
+        DataStreamSource<String> streamSource = env.readTextFile(inputPath);
 
-        AggregateOperator<Tuple2<String, Integer>> sum = stringDataSource.flatMap(new MyFlatMapper())
-                .groupBy(0).sum(1);
-
+//        fields: tuple位置
+        DataStream<Tuple2<String, Integer>> sum = streamSource.flatMap(new MyFlatMapper())
+                .keyBy(0)
+                .sum(1);
         sum.print();
+//      执行任务
+        sum.executeAndCollect();
+
+//        线程编号(默认并行度，物理机的cup Num) > (fieldKey, sumNum）
+//        2> (sfesef,1)
+//        3> (sfe,1)
+//        9> (,1)
+//        12> (classworlds,1)
+//        10> (baseDirFound,1)
+//        5> (asefs,1)
+//        7> (sfeclassworlds,1)
+//        10> (fesfs,1)
+//        12> (classworlds,2)
+//        9> (,2)
+//        12> (classworlds,3)
+//        7> (s,1)
+//        12> (classworlds,4)
+//        3> (sfe,2)
+//        12> (classworlds,5)
+//        3> (sfe,3)
+//        4> (hello,1)
+//        9> (,3)
+//        5> (asefs,2)
+//        9> (,4)
+//        9> (,5)
+//        12> (classworlds,6)
+//        4> (hello,2)
+//        12> (classworlds,7)
+//        5> (asefs,3)
+
 
     }
 
     public static class MyFlatMapper implements FlatMapFunction<String, Tuple2<String, Integer>> {
-
 
         @Override
         public void flatMap(String s, Collector<Tuple2<String, Integer>> collector) throws Exception {
